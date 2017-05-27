725907cb874f2262d42e65eb96f008018ceb796e
yangjian <702021120@qq.com>
2017-05-28 00:47:43 +0800

    主页 创建房间

--- app.js
+++ app.js
@@ -1,60 +1,6 @@
-var config = require('./config');
 App({
   onLaunch: function () {
-    var self = this;
-    wx.login({
-      success: function (data) {
-        console.log('step1 success 调用微信登录接口');
-
-        wx.request({
-          url: "https://api.weixin.qq.com/sns/jscode2session?appid=" + config.appId + "&secret=" + config.appSecret + "&js_code=" + data.code + "&grant_type=authorization_code",
-          data: {
-            code: data.code
-          },
-          success: function (res) {
-            getApp().user.openid = res.data.openid;
-            console.log('step2.0 success 获取微信openid ：', getApp().user.openid);
-
-            wx.request({
-              url: `https://api.weixin.qq.com/cgi-bin/token`,
-              data: {
-                grant_type: 'client_credential',
-                appid: config.appId,
-                secret: config.appSecret,
-              },
-
-              success: function (result) {
-                console.log('step3 success 获取用户access_token：' + result.data.access_token+'，初始化完成');
-                getApp().user.accessToken = result.data.access_token;
-              },
-
-              fail: function ({errMsg}) {
-                console.log('step3 fail 获取用户access_token', errMsg);
-              }
-            })
-          },
-
-          fail: function (res) {
-            console.log('step2.0 fail 获取微信openid，将无法正常使用开放接口等服务', res);
-          }
-        })
-
-        wx.getUserInfo({
-          success: function (res) {
-            console.log('step2.1 success 获取微信用户信息 ' ,res.userInfo);
-            getApp().user.userInfo = res.userInfo;
-          },
-
-          fail: function (res) {
-            console.log('step2.1 fail 获取微信用户信息', res);
-          }
-        })
-      },
-      
-      fail: function (err) {
-        console.log('step1 fail 调用微信登录接口，将无法正常使用开放接口等服务', err);
-      }
-    })
+    
   },
 
   user:{
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,68 @@
+var config = require('../../config');
 Page({
   data: {
-    imgUrls: [
-      'http://120.76.241.240/image_index/img_ad_5.jpg',
-      'http://120.76.241.240/image_index/img_ad_1.jpg',
-      'http://120.76.241.240/image_index/img_ad_2.jpg',
-      'http://120.76.241.240/image_index/img_ad_3.jpg',
-      'http://120.76.241.240/image_index/img_ad_4.jpg',
-      'http://120.76.241.240/image_index/img_ad_6.jpg',
-    ],
+    userInfo:{}
+  },
+
+  onLoad: function (){
+    var self = this;
+    wx.login({
+      success: function (data) {
+        console.log('step1 success 调用微信登录接口');
+
+        wx.request({
+          url: "https://api.weixin.qq.com/sns/jscode2session?appid=" + config.appId + "&secret=" + config.appSecret + "&js_code=" + data.code + "&grant_type=authorization_code",
+          data: {
+            code: data.code
+          },
+          success: function (res) {
+            getApp().user.openid = res.data.openid;
+            console.log('step2.0 success 获取微信openid ：', getApp().user.openid);
+
+            wx.request({
+              url: `https://api.weixin.qq.com/cgi-bin/token`,
+              data: {
+                grant_type: 'client_credential',
+                appid: config.appId,
+                secret: config.appSecret,
+              },
+
+              success: function (result) {
+                console.log('step3 success 获取用户access_token：' + result.data.access_token + '，初始化完成');
+                getApp().user.accessToken = result.data.access_token;
+              },
+
+              fail: function ({errMsg}) {
+                console.log('step3 fail 获取用户access_token', errMsg);
+              }
+            })
+          },
+
+          fail: function (res) {
+            console.log('step2.0 fail 获取微信openid，将无法正常使用开放接口等服务', res);
+          }
+        })
+
+        wx.getUserInfo({
+          success: function (res) {
+            console.log('step2.1 success 获取微信用户信息 ', res.userInfo);
+            getApp().user.userInfo = res.userInfo;
+            self.data.userInfo = getApp().user.userInfo;
+            self.setData({
+              userInfo: self.data.userInfo
+            })
+          },
+
+          fail: function (res) {
+            console.log('step2.1 fail 获取微信用户信息', res);
+          }
+        })
+      },
+
+      fail: function (err) {
+        console.log('step1 fail 调用微信登录接口，将无法正常使用开放接口等服务', err);
+      }
+    })
   },
 
   onShow: function () {
