b299d12ad8650c1352c87e23773b489c44db07b7
yangjian <702021120@qq.com>
2017-06-01 13:05:06 +0800

    test

--- pages/manageGame/manageGame.js
+++ pages/manageGame/manageGame.js
@@ -4,118 +4,8 @@ var gameId;
 var tmp_i;
 Page({
   data: {
-    roleList: [
-      // { 
-      //   "gameId": 163, 
-      //   "playerAvatarUrl": 
-      //   "http://wx.qlogo.cn/mmopen/vi_32/GvzrAKyDiboTQJWicAKm5ejicUcMB9txkW9aApDBgvG8avfeA82p6b2ghskI02IJuC2RM1NdzvCAXaCHuACK6oAOQ/0", 
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4", 
-      //   "playerNickName": "被人追杀的eason", 
-      //   "roleId": 839, 
-      //   "roleType": 3, 
-      //   "death": 0,
-      //   "victory": 0 
-      // },
-      // {
-      //   "gameId": 163,
-      //   "playerAvatarUrl":
-      //   "http://wx.qlogo.cn/mmopen/vi_32/hzgRzL39o1b1NVPlhfoFZqapqUNUvnlkKhcIshXkTkxDpJdHnh3LMz7g1eQbhUBnicx2mwndpduiazS39phM6ekg/0",
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4",
-      //   "playerNickName": "习大大喵喵",
-      //   "roleId": 840,
-      //   "roleType": 0,
-      //   "death": 1,
-      //   "victory": 0
-      // },
-      // {
-      //   "gameId": 163,
-      //   "playerAvatarUrl":
-      //   "http://wx.qlogo.cn/mmopen/vi_32/GvzrAKyDiboTQJWicAKm5ejicUcMB9txkW9aApDBgvG8avfeA82p6b2ghskI02IJuC2RM1NdzvCAXaCHuACK6oAOQ/0",
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4",
-      //   "playerNickName": "杨窝窝",
-      //   "roleId": 841,
-      //   "roleType": 0,
-      //   "death": 0,
-      //   "victory": 0
-      // },
-      // {
-      //   "gameId": 163,
-      //   "playerAvatarUrl":
-      //   "http://wx.qlogo.cn/mmopen/vi_32/5YSF1tBkafDQGIIic9uUqsc3PwYUcoCQWNbD1rD3OlZmEYVSOBFG0UkYNibEUHyxh1icvmQOysqm1zVJdDFzeeUiag/0",
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4",
-      //   "playerNickName": "my c 柱子",
-      //   "roleId": 842,
-      //   "roleType": 1,
-      //   "death": 0,
-      //   "victory": 0
-      // },
-      // {
-      //   "gameId": 163,
-      //   "playerAvatarUrl":
-      //   "http://wx.qlogo.cn/mmopen/vi_32/GvzrAKyDiboTQJWicAKm5ejicUcMB9txkW9aApDBgvG8avfeA82p6b2ghskI02IJuC2RM1NdzvCAXaCHuACK6oAOQ/0",
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4",
-      //   "playerNickName": "大水怪123",
-      //   "roleId": 843,
-      //   "roleType": 1,
-      //   "death": 1,
-      //   "victory": 0
-      // },
-      // {
-      //   "gameId": 163,
-      //   "playerAvatarUrl":
-      //   "http://wx.qlogo.cn/mmopen/vi_32/GvzrAKyDiboTQJWicAKm5ejicUcMB9txkW9aApDBgvG8avfeA82p6b2ghskI02IJuC2RM1NdzvCAXaCHuACK6oAOQ/0",
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4",
-      //   "playerNickName": "my c 锅子",
-      //   "roleId": 844,
-      //   "roleType": 2,
-      //   "death": 0,
-      //   "victory": 0
-      // },
-      // {
-      //   "gameId": 163,
-      //   "playerAvatarUrl":
-      //   "http://wx.qlogo.cn/mmopen/vi_32/hzgRzL39o1b1NVPlhfoFZqapqUNUvnlkKhcIshXkTkxDpJdHnh3LMz7g1eQbhUBnicx2mwndpduiazS39phM6ekg/0",
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4",
-      //   "playerNickName": "川岛芳子",
-      //   "roleId": 845,
-      //   "roleType": 2,
-      //   "death": 0,
-      //   "victory": 0
-      // },
-      // {
-      //   "gameId": 163,
-      //   "playerAvatarUrl":
-      //   "http://wx.qlogo.cn/mmopen/vi_32/GvzrAKyDiboTQJWicAKm5ejicUcMB9txkW9aApDBgvG8avfeA82p6b2ghskI02IJuC2RM1NdzvCAXaCHuACK6oAOQ/0",
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4",
-      //   "playerNickName": "大黄鱼君",
-      //   "roleId": 846,
-      //   "roleType": 2,
-      //   "death": 2,
-      //   "victory": 0
-      // },
-      // {
-      //   "gameId": 163,
-      //   "playerAvatarUrl":
-      //   "http://wx.qlogo.cn/mmopen/vi_32/5YSF1tBkafDQGIIic9uUqsc3PwYUcoCQWNbD1rD3OlZmEYVSOBFG0UkYNibEUHyxh1icvmQOysqm1zVJdDFzeeUiag/0",
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4",
-      //   "playerNickName": "皇上的小马褂",
-      //   "roleId": 847,
-      //   "roleType": 2,
-      //   "death": 2,
-      //   "victory": 0
-      // },
-      // {
-      //   "gameId": 163,
-      //   "playerAvatarUrl":
-      //   "http://wx.qlogo.cn/mmopen/vi_32/GvzrAKyDiboTQJWicAKm5ejicUcMB9txkW9aApDBgvG8avfeA82p6b2ghskI02IJuC2RM1NdzvCAXaCHuACK6oAOQ/0",
-      //   "playerId": "oNMwb0dFoGQ7TvHJd7jbyFdmjgk4",
-      //   "playerNickName": "芳芳芳",
-      //   "roleId": 848,
-      //   "roleType": 2,
-      //   "death": 0,
-      //   "victory": 0
-      // },
-    ]
+    scrollHeight: 900,
+    roleList: []
   },
 
   onLoad: function (options) {
@@ -124,7 +14,14 @@ Page({
     wx.setNavigationBarTitle({
       title: "我是裁判（" + options.inviteCode +"）",
     })
-    
+    wx.getSystemInfo({
+      success: function (res) {
+        console.log(res);
+        self.setData({
+          scrollHeight: res.windowHeight - 50,
+        });
+      }
+    });
     self.getRoleList();
   },
 
